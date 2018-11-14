206872d2b77b9b983a28c0b02d04185b3dc83b97
Vincent <44287355+PainOlait@users.noreply.github.com>
2018-11-14 17:39:48 +0100

    animation lumière

--- js_membres.js
+++ js_membres.js
@@ -2,12 +2,32 @@
 $(document).ready(function()
 {
 
+  var light = 0;
+
   $('#N').click(function(){
+    if (light == 0) {
+      lightOn();
+      light = 1;
+    }
+    else {
+      lightOff();
+      light = 0;
+    }
+
+  });
+
+  function lightOn(){
     $('#corp').addClass('active');
-    console.log("hello");
     $('body').css('background', 'white');
     $('#corp .member').css('width', 'calc(100%/14)');
-  });
+    $('.cls-1').css('fill', '#4ec2f0');
+  }
+
+  function lightOff(){
+    $('#corp').removeClass('active');
+    $('body').css('background', '#2B2E31');
+    $('.cls-1').css('fill', '#ffffff');
+  }
 
   $('#corp .member').click(function(){
 
