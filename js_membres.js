a8a9e567986ac21f65a04349910a407580163c1a
Vincent <44287355+PainOlait@users.noreply.github.com>
2018-11-14 14:48:27 +0100

    Avancée !

--- js_membres.js
+++ js_membres.js
@@ -5,6 +5,13 @@ $(document).ready(function()
   $('#N').click(function(){
     $('#corp').addClass('active');
     console.log("hello");
+    $('body').css('background', 'white');
+    $('#corp .member').css('width', 'calc(100%/14)');
   });
+
+  $('#corp .member').click(function(){
+
+    console.log("world");
+  })
 
 });
